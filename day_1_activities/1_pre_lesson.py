395a10f5dc837340368081a4a3694150be33554e
emivega16 <emivega316@gmail.com>
2025-12-15 14:07:58 -0600

    Update 1_pre_lesson.py

--- day_1_activities/1_pre_lesson.py
+++ day_1_activities/1_pre_lesson.py
@@ -1,1 +1,35 @@
-# this is what we will use for the video intro to dictionaries
+# this is what we will use for the video intro to dictionaries = a collection of (key:value) pairs ordered and changeable. No duplictaes
+
+capitals = {"USA": "Washington D.C.",
+            "India": "New Dehli", 
+            "China": "Beijing",
+            "Russia": "Moscow"}
+
+#print(dir(capitals))
+#print(help(capitals))
+print(capitals.get("Japan"))
+
+if capitals.get("Russia"):
+    print("That capital exists")
+else:
+    print("That capital doesn't exist")
+
+capitals.update({"Germany": "Berlin"})
+capitals.update({"USA": "Detroit"})
+capitals.popitem()
+
+keys = capitals.keys()
+
+for key in capitals.keys():
+    print(key)
+
+values = capitals.values()
+for value in capitals.values():
+    print(value)
+
+print(capitals)
+
+
+items = capitals.items()
+for key, value in capitals.items():
+    print(f"{key}: {value}")
